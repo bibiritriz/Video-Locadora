f8e8e6b045d7f387f012cc75216438c7712bf08d
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-06-30 09:43:10 -0300

    :tada: first commit

---
+++ src/main/java/br/gov/sp/itu/fatec/videolocaldora/VideolocaldoraApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.itu.fatec.videolocaldora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideolocaldoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideolocaldoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/itu/fatec/videolocaldora/VideolocaldoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.itu.fatec.videolocaldora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideolocaldoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
